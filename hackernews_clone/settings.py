005117a220277e85329d706aa4ada0068624c3fa
hiki0505 <hikmet.pirmamedov@mail.ru>
2022-03-31 02:12:47 +0400

    made some changes in settings to avoid error message on starting page

--- hackernews_clone/settings.py
+++ hackernews_clone/settings.py
@@ -21,8 +21,8 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DOCKER_PROD_FLAG = False
-HEROKU_PROD_FLAG = True
+DOCKER_PROD_FLAG = True
+HEROKU_PROD_FLAG = False
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
