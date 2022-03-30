35d61798001455b2671240468c0711e2a4e57637
hiki0505 <hikmet.pirmamedov@mail.ru>
2022-03-31 00:41:20 +0400

    final changes in settings

--- hackernews_clone/settings.py
+++ hackernews_clone/settings.py
@@ -168,4 +168,5 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-django_heroku.settings(locals())
+if HEROKU_PROD_FLAG:
+    django_heroku.settings(locals())
